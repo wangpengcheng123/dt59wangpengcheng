8e41aa8802efada81f48c3cdb3c4522a2d5b4a7a
John <John@192.168.1.78>
2017-12-20 19:41:06 +0800

    aa

---
+++ src/main/java/dt59wangpengcheng/aa.java
@@ -0,0 +1,20 @@
+/**
+ * Project Name:dt59wangpengcheng
+ * File Name:aa.java
+ * Package Name:dt59wangpengcheng
+ * Date:2017年12月20日下午7:08:30
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package dt59wangpengcheng;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月20日 下午7:08:30 <br/>
+ * @author   John
+ * @version
+ * @see
+ */
+public class aa {
+
+}
+
